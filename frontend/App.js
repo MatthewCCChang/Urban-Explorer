34b63d52bda469e4ea25de67584982707a0ba14c
Mina Yoon <ijhamin0320@gmail.com>
2024-04-20 10:55:53 -0700

    login?

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from "react";
+// import Login from "./Login";
 
-export default function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Login />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from "react-native/Libraries/NewAppScreen";
+const App = () => {
+  const onPressLogin = () => {
+    // Do something about login operation
+  };
+  const onPressForgotPassword = () => {
+    // Do something about forgot password operation
+  };
+  const onPressSignUp = () => {
+    // Do something about signup operation
+  };
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}> Urban Explore</Text>
+      <Image
+        source={require('./assets/explore-logo.png')}
+        style={styles.image}
+      />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setState({ email: text })}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          secureTextEntry
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setState({ password: text })}
+        />
+      </View>
+      <TouchableOpacity onPress={onPressForgotPassword}>
+        <Text style={styles.forgotAndSignUpText}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onPressSignUp}>
+        <Text style={styles.forgotAndSignUpText}>Signup</Text>
+      </TouchableOpacity>
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#a2e5e8",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  title: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#0f3d40",
+    marginBottom: 40,
+  },
+  image: {
+    marginBottom: 30
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#3AB4BA",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+  forgotAndSignUpText: {
+    color: "#0f3d40",
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#0f3d40",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "white"
+  }
 });
+export default App;
