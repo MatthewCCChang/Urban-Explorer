56927ea8c6f17b98f72052a42c92e593bf1e3227
Mina Yoon <ijhamin0320@gmail.com>
2024-04-20 23:11:22 -0700

    minor change

---
+++ frontend/auth0-credentials.js
@@ -0,0 +1,5 @@
+// create auth0-credentials.js
+const AUTH0_DOMAIN = "dev-ww4iy0dflry18rgg.us.auth0.com"; // Replace with your Auth0 domain
+const AUTH0_CLIENT_ID = "cYFl3feUTS9Vgl2OVvDlmrnfypZu4ceZ"; // Replace with your Auth0 client ID
+
+export { AUTH0_DOMAIN, AUTH0_CLIENT_ID };
